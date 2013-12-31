166f25f89244e170ccbe895e31398d4ce3270d99
nitesh kumar-xft <nitesh_kumar-xft@external.mckinsey.com>
2013-12-31 22:11:33 +0530

    restricting the view to 20 and randomizign the view using Fisher–Yate algorith

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,23 @@
 function cityCtrl($scope, $http) {
   $http.get('stations/stationcode.json').success(function(data) {
     $scope.cities = data;
+    shuffleArray($scope.cities);
   });
+}
+//$('.city-list').jscroll();
+var shuffleArray = function(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
 }
